906eeb11ed1495259b63d08dd11d11780f396be1
Mohamad Sabra <mrs23@mail.aub.edu>
2022-03-09 16:14:02 +0200

    Update map.dart

--- lib/Map/map.dart
+++ lib/Map/map.dart
@@ -368,7 +368,7 @@ class _MapPage extends State<MapPage> {
           minMaxZoomPreference: MinMaxZoomPreference(8.5, 20),
           cameraTargetBounds: CameraTargetBounds(LatLngBounds(
               northeast: LatLng(34.6566324, 36.6896525),
-              southwest: LatLng(33.0569738, 35.0935346))),
+              southwest: LatLng(33.4569738, 35.4935346))),
           initialCameraPosition: _initialCameraPosition,
           myLocationButtonEnabled: false,
           zoomControlsEnabled: false,
