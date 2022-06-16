0c18a3a7c509a49a0032a7f1eea3aafa651341f0
Mohamad Sabra <mrs23@mail.aub.edu>
2022-06-16 13:41:34 +0300

    removed login features

--- lib/Backend/Login.dart
+++ lib/Backend/Login.dart
@@ -1,128 +1,128 @@
-//Base Login Page
+// //Base Login Page
 
-// ignore_for_file: file_names, unused_import, use_key_in_widget_constructors, prefer_typing_uninitialized_variables, unnecessary_this, prefer_const_constructors, avoid_unnecessary_containers, duplicate_ignore
+// // ignore_for_file: file_names, unused_import, use_key_in_widget_constructors, prefer_typing_uninitialized_variables, unnecessary_this, prefer_const_constructors, avoid_unnecessary_containers, duplicate_ignore
 
-import 'dart:io';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:interactive_map/Repos/UserRepo.dart';
-import 'package:interactive_map/Repos/media.dart';
-import 'package:interactive_map/Homepages/mainPage.dart';
-import 'package:interactive_map/Homepages/mainPageGuest.dart';
-import 'package:interactive_map/Map/map.dart';
+// import 'dart:io';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+// import 'package:interactive_map/Repos/UserRepo.dart';
+// import 'package:interactive_map/Repos/media.dart';
+// import 'package:interactive_map/Homepages/mainPage.dart';
+// import 'package:interactive_map/Homepages/mainPageGuest.dart';
+// import 'package:interactive_map/Map/map.dart';
 
-import 'auth.dart';
+// import 'auth.dart';
 
-class LoginPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // ignore: prefer_const_constructors
-        title: Center(child: Text('Deal With Past')),
-      ),
-      body: Body(),
-    );
-  }
-}
+// class LoginPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         // ignore: prefer_const_constructors
+//         title: Center(child: Text('Deal With Past')),
+//       ),
+//       body: Body(),
+//     );
+//   }
+// }
 
-class Body extends StatefulWidget {
-  @override
-  _BodyState createState() => _BodyState();
-}
+// class Body extends StatefulWidget {
+//   @override
+//   _BodyState createState() => _BodyState();
+// }
 
-class _BodyState extends State<Body> {
-  UserRepo userRepo = UserRepo();
-  Media media = Media();
-  late var token;
+// class _BodyState extends State<Body> {
+//   UserRepo userRepo = UserRepo();
+//   Media media = Media();
+//   late var token;
 
-  @override
-  void initState() {
-    super.initState();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
 
-  Future<void> click() async {
-    token = await userRepo.Authenticate("admin", "admin_1234");
-    signInWithGoogle().then((user) async => {});
-  }
+//   Future<void> click() async {
+//     token = await userRepo.Authenticate("admin", "admin_1234");
+//     signInWithGoogle().then((user) async => {});
+//   }
 
-  Widget loginButton() {
-    // ignore: deprecated_member_use
-    return OutlineButton(
-        onPressed: this.click,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-        splashColor: Colors.grey[600],
-        borderSide: BorderSide(color: Colors.grey),
-        child: Container(
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: const <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(left: 10),
-                  child: Text(
-                    'سجل مع Google',
-                    style: TextStyle(color: Colors.grey, fontSize: 25),
-                  ),
-                ),
-                Image(image: AssetImage('assets/google_logo.png'), height: 35),
-              ],
-            ),
-          ),
-        ));
-  }
+//   Widget loginButton() {
+//     // ignore: deprecated_member_use
+//     return OutlineButton(
+//         onPressed: this.click,
+//         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+//         splashColor: Colors.grey[600],
+//         borderSide: BorderSide(color: Colors.grey),
+//         child: Container(
+//           child: Padding(
+//             padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+//             child: Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               mainAxisSize: MainAxisSize.min,
+//               children: const <Widget>[
+//                 Padding(
+//                   padding: EdgeInsets.only(left: 10),
+//                   child: Text(
+//                     'سجل مع Google',
+//                     style: TextStyle(color: Colors.grey, fontSize: 25),
+//                   ),
+//                 ),
+//                 Image(image: AssetImage('assets/google_logo.png'), height: 35),
+//               ],
+//             ),
+//           ),
+//         ));
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        color: Colors.white,
-        child: Center(
-            child: Column(
-          children: <Widget>[
-            SizedBox(height: 200),
-            Text(
-              "Deal With Past",
-              style: TextStyle(color: Colors.black, fontSize: 45),
-            ),
-            SizedBox(height: 120),
-            Container(child: loginButton()),
-            SizedBox(height: 30),
-            Container(
-                // ignore: deprecated_member_use
-                child: OutlineButton(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15)),
-              splashColor: Colors.grey[600],
-              borderSide: BorderSide(color: Colors.grey),
-              child: Container(
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(25, 15, 25, 15),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: const <Widget>[
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(35, 0, 35, 0),
-                        child: Text(
-                          'أكمل كضيف',
-                          style: TextStyle(color: Colors.grey, fontSize: 25),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              onPressed: () {
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => WelcomePageGuest(),
-                    ));
-              },
-            )),
-          ],
-        )));
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//         color: Colors.white,
+//         child: Center(
+//             child: Column(
+//           children: <Widget>[
+//             SizedBox(height: 200),
+//             Text(
+//               "Deal With Past",
+//               style: TextStyle(color: Colors.black, fontSize: 45),
+//             ),
+//             SizedBox(height: 120),
+//             Container(child: loginButton()),
+//             SizedBox(height: 30),
+//             Container(
+//                 // ignore: deprecated_member_use
+//                 child: OutlineButton(
+//               shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(15)),
+//               splashColor: Colors.grey[600],
+//               borderSide: BorderSide(color: Colors.grey),
+//               child: Container(
+//                 child: Padding(
+//                   padding: EdgeInsets.fromLTRB(25, 15, 25, 15),
+//                   child: Row(
+//                     mainAxisAlignment: MainAxisAlignment.center,
+//                     mainAxisSize: MainAxisSize.min,
+//                     children: const <Widget>[
+//                       Padding(
+//                         padding: EdgeInsets.fromLTRB(35, 0, 35, 0),
+//                         child: Text(
+//                           'أكمل كضيف',
+//                           style: TextStyle(color: Colors.grey, fontSize: 25),
+//                         ),
+//                       )
+//                     ],
+//                   ),
+//                 ),
+//               ),
+//               onPressed: () {
+//                 Navigator.pushReplacement(
+//                     context,
+//                     MaterialPageRoute(
+//                       builder: (context) => WelcomePageGuest(),
+//                     ));
+//               },
+//             )),
+//           ],
+//         )));
+//   }
+// }
--- lib/Backend/auth.dart
+++ lib/Backend/auth.dart
@@ -1,35 +1,35 @@
-// ignore_for_file: unused_import, unnecessary_null_comparison
+// // ignore_for_file: unused_import, unnecessary_null_comparison
 
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
 
-final FirebaseAuth _auth = FirebaseAuth.instance;
-final GoogleSignIn googleSignIn = GoogleSignIn();
+// final FirebaseAuth _auth = FirebaseAuth.instance;
+// final GoogleSignIn googleSignIn = GoogleSignIn();
 
-Future<User?> signInWithGoogle() async {
-  final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
-  final GoogleSignInAuthentication googleSignInAuthentication =
-      await googleSignInAccount!.authentication;
+// Future<User?> signInWithGoogle() async {
+//   final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+//   final GoogleSignInAuthentication googleSignInAuthentication =
+//       await googleSignInAccount!.authentication;
 
-  final AuthCredential credential = GoogleAuthProvider.credential(
-      idToken: googleSignInAuthentication.idToken,
-      accessToken: googleSignInAuthentication.accessToken);
+//   final AuthCredential credential = GoogleAuthProvider.credential(
+//       idToken: googleSignInAuthentication.idToken,
+//       accessToken: googleSignInAuthentication.accessToken);
 
-  final UserCredential authResult =
-      await _auth.signInWithCredential(credential);
-  final User? user = authResult.user;
+//   final UserCredential authResult =
+//       await _auth.signInWithCredential(credential);
+//   final User? user = authResult.user;
 
-  assert(!user!.isAnonymous);
-  assert(await user!.getIdToken() != null);
+//   assert(!user!.isAnonymous);
+//   assert(await user!.getIdToken() != null);
 
-  final User? currentUser = _auth.currentUser;
-  assert(currentUser!.uid == user!.uid);
+//   final User? currentUser = _auth.currentUser;
+//   assert(currentUser!.uid == user!.uid);
 
-  return user;
-}
+//   return user;
+// }
 
-signOut() async {
-  await googleSignIn.signOut();
-  await _auth.signOut();
-}
+// signOut() async {
+//   await googleSignIn.signOut();
+//   await _auth.signOut();
+// }
